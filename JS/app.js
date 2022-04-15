bd403eeea4b78e0d0b7e4386ded41973888416b3
Iair Sendrowicz <iachusend@gmail.com>
2022-04-15 15:09:45 -0300

    Crear un algoritmo creando un ciclo

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+let comida = prompt("Ingresá la comida que te gusta");
+
+while(comida != "esc") {
+    switch(comida) {
+        case "sushi":
+            alert("¡La Mejor comida!");
+            break;   
+        case "pizza":
+            alert("¡MMM, que rico!");
+            break;
+        case "Brocoli":
+            alert("¡Que asco!");
+            break;
+    }
+    entreda = prompt("Ingresá la comida que te gusta");
+}
