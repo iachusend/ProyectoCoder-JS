2f06cfb939eaebf11c159991330ddd1119313a32
Iair Sendrowicz <iachusend@gmail.com>
2022-04-25 22:15:12 -0300

    Desafío: Incorporar Arrays

--- JS/app.js
+++ JS/app.js
@@ -1,69 +1,9 @@
-let cantidadPlata = 0;
-
-pedirCredito()
-
-function pedirCredito(){
-    let prestamo = confirm("Ingrese Usuario")
-    ingresarUsuario(prestamo)
+let inicio = prompt("Ingresar comida");
+const equipo = [];
+while (inicio != 'salir') {
+    equipo.push(inicio);
+    inicio = prompt("Ingresar comida");
 }
-
-function ingresarUsuario(us){
-    if(us){
-        let usuario = prompt("Ingrese su usuario");
-        validarUsuario(usuario)
-    }else {
-        alert("Es necesario ingresar el usuario")
-    }
-}
-
-function validarUsuario(u){
-    if(u === "coder"){
-        seleccionarCredito()
-    }else {
-        alert("Usuario incorrecto")
-    }
-    ingresarUsuario()
-}
-
-function seleccionarCredito() {
-    console.log("1: Pedir prestamo");
-    console.log("2: Ver cuenta")
-    console.log("3: Salir de la cuenta")
-    console.log("------------------------")
-    
-    let opc = prompt("Ingresar Opcion")
-    switch(opc){
-        case "1":
-            let cantidad = Number(prompt("Cantidad pedida"))
-            montoPedido(cantidad)
-            break;
-        case "2": 
-            consultarSaldoCuenta()
-            break;
-        case "3": 
-            salir();
-            break;
-        default:
-            console.log("Peticion Incorrecta")
-            break;
-    }
-}
-
-function montoPedido(plata) {
-    if(plata > 10000){
-        alert("Se ha exedido del monto maximo que se puede solicitar")
-    } else {
-        alert("retiraste " + plata + " pesos")
-    }
-    seleccionarCredito()
-}
-
-function consultarSaldoCuenta() {
-    alert("Actualmente tiene ingresados " + cantidadPlata + " pesos")
-    seleccionarCredito()
-}
-
-function salir() {
-    alert("Usted ha finalizado sus operacion bancarias")
-    seleccionarCredito()
+for (let i = 0; i < equipo.length; i+=1) {
+    alert(`Se encuentra en la posición ${i} el jugador ${equipo[i]}`);
 }
